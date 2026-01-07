2a9aee66522bf9b196ede1bc3aed0971ae23acb4
Pratyush Kumar <134200485+Pratyush-95@users.noreply.github.com>
2026-01-07 13:12:16 +0530

    first commit

---
+++ Practice1.js
@@ -0,0 +1,10 @@
+// write a code to reverse a string
+
+let str= "hello to javascript";
+console.log(str);
+console.log(str.length);
+let reverse = str.split(" ").reverse().join(" ");
+console.log(reverse);
+
+
+

2070208bc88cbf6041185f97a3ba0bef602b0645
Pratyush Kumar <134200485+Pratyush-95@users.noreply.github.com>
2026-01-24 23:00:03 +0530

    Practice: even and odd number program

---
+++ Practice2.js
@@ -0,0 +1,16 @@
+// Ye function check karta hai ki diya gaya number even hai ya odd
+
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    console.log("Even Number");
+  } 
+  else {
+    console.log("Odd Number");
+  }
+}
+
+// Function call 
+checkEvenOdd(10);   // Output: Even Number
+
+// Function call 
+checkEvenOdd(7);    // Output: Odd Number
